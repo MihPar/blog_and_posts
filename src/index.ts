765770e83d712d5e06830274c2e9ef91390d0859
MihPar <9496260@gmail.com>
2023-09-22 17:10:34 +0300

    22.10

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { postsRouter } from './router/posts_router'
 import { blogsRouter } from './router/blogs_router'
 import { deletedAllRouter } from './router/deletedAllRouter'
 
-export const app = express()
+const app = express()
 
 app.use(bodyParser())
 app.use('/posts', postsRouter)
